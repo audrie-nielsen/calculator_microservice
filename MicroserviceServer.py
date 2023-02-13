28a93cf32e58c0c91dd8366125245e0dbeaebefe
audrie-nielsen <122310331+audrie-nielsen@users.noreply.github.com>
2023-02-12 16:59:11 -0800

    Update MicroserviceServer.py

--- MicroserviceServer.py
+++ MicroserviceServer.py
@@ -1,40 +1,35 @@
 import time
 import zmq
+import json
 
 context = zmq.Context()
 socket = context.socket(zmq.REP)
 socket.bind("tcp://*:5555")
 
-resArray = []
-equArray = []
+arr = []
 
 while True:
     #  Waits for next request from client
     req = socket.recv()
     print("Received request: %s" % req)
-
     time.sleep(1)
 
-    #  If request is "POP EQUATION" or "POP RESULT", server sends back last result PUSHed to that stack
-    if req == b"POP EQUATION":
-        if equArray == []:
-            socket.send(b"The equation backlog is currently empty.")
+    #  If request is "POP", server sends back a JSON containing the last pushed equation and result
+    if req == b"POP":
+        if arr == []:
+            resJSON = {"equation": "empty", "result": "empty"}
+            res = json.dumps(resJSON)
+            socket.send_json(res)
         else:
-            res = equArray[-1]
-            equArray.pop()
-            socket.send(res)
-    elif req == b"POP RESULT":
-        if resArray == []:
-            socket.send(b"The result backlog is currently empty.")
-        else:
-            res = resArray[-1]
-            resArray.pop()
-            socket.send(res)
-    else:  # Checks if request is an equation or a result
-        str = req.decode('ASCII')
-        if ' + ' in str or ' - ' in str or ' / ' in str or ' * ' in str:
-            equArray.append(req)
-            socket.send(b"Result added to the equation stack")
-        else:
-            resArray.append(req)
-            socket.send(b"Result added to the result stack")
+            result = arr[-1].decode("ASCII")
+            arr.pop()
+            equation = arr[-1].decode("ASCII")
+            arr.pop()
+
+            resJSON = {"equation": equation, "result": result}
+            res = json.dumps(resJSON)
+            socket.send_json(res)
+    # Else, adds the request data to the array
+    else:
+        arr.append(req)
+        socket.send(b"Result added to the stack")
